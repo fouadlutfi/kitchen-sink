cd9811177f4ba92ad197471e99e6ffdda7e704d8
fouad lutfi <thefouad@gmail.com>
2018-06-07 02:23:02 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+var firstname = "fouad";
+
+function capitalizeFirstLetter(string) {
+    return string,charAt(0).toUpperCase() + string.slice (1);
+}
+
+console.log (capitalizeFirstLetter(firstname));
+const stateNumber = 13;
+
+var Quanity = 5 + 4
+
+if (firstName.charAt(0) > "L") {
+    alert("back of the line!");}
+else { alert( "Your Next!! ");}
+
+function sayhello () { alert ( "hello world ");
+}
+if  (age < 21) {
+    alert("Sorry, " + name + ", you aren't old enough to view this page!");
+ }
+else {
+    alert("You can smoke weed in Colorado!"); 
+}
+
+console.log(checkage("johnny" ,23))
+console.log(checkage("cash" ,28))
+console.log(checkage("elvis" ,16))
+console.log(checkage("jackson" ,24))
+ 
+var favveggies = ['Corn','Ocra','Tomato','Squash','Greenbeans']
+
+for (i = 0; i < faveVeggies.length; i++){
+    console.log(faveVeggies[i]);
+}
+
+var people = [
+    {name: "wesley",age: 63},
+
+    {name: "brian",age: 29},
+
+    {name: "daniel",age: 3},
+  
+    {name: "andrew",age: 17},
+
+    {name: "paul",age: 63},
+];
+
+for (i = 0; i < people.length; i++) {
+    checkAge(people[i].name, people[i].age);
+}
+function getlength(word) {
+    return word.length;
+}
+var number = ("Hello World!")
+let anyWord = getLength('Hello World');
+let test = anyWord % 5
+
+if (test == 0) {
+    console.log('The world is nice and even');
+}
+else {
+    console.log('The world is an odd place');
+}
